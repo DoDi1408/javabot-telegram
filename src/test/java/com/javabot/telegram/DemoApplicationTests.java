7a6122aa7429082507b9538e3bad7c6cfd4e4191
DoDi1408 <ro.chavez.5454@gmail.com>
2024-03-20 17:27:59 -0600

    testing something, perchance i am close to the solution

--- src/test/java/com/javabot/telegram/DemoApplicationTests.java
+++ src/test/java/com/javabot/telegram/DemoApplicationTests.java
@@ -8,24 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 	
-	@Test
-    void testGetEnvironmentVariable() {
-		// Get all environment variables
-		Map<String, String> envMap = System.getenv();
-		// Get specific variables
-		String directory = envMap.get("DIRECTORY");
-		String dbuser = envMap.get("DATABASE_USER");
-		String dbpwd = envMap.get("DATABASE_PWD");
-		String dbuser2 = envMap.get("DB_CREDENTIALS_USR");
-		String dbpwd2 = envMap.get("DB_CREDENTIALS_PSW");
-        // Print the values
-		System.out.println("DIRECTORY: " + directory);
-		System.out.println("dbuser: " + dbuser);
-		System.out.println("dbpwd: " + dbpwd);
-		System.out.println("dbuser: " + dbuser2);
-		System.out.println("dbpwd: " + dbpwd2);
-
-    }
 
     @Test
 	void contextLoads() {
