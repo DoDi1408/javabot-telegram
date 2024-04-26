379583357dee61cc51eed953f2105b265ca8101d
DoDi1408 <ro.chavez.5454@gmail.com>
2024-04-26 15:46:02 -0600

    switched to long polling

--- src/main/java/com/javabot/TelegramBotTeam23Application.java
+++ src/main/java/com/javabot/TelegramBotTeam23Application.java
@@ -1,30 +1,31 @@
 package com.javabot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import com.javabot.controller.Team23BotController;
 
 
 
 @SpringBootApplication
-public class TelegramBotTeam23Application {
+public class TelegramBotTeam23Application implements CommandLineRunner{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotTeam23Application.class, args);
 	}
-	/*
-	private String botName = System.getenv("BOT_CREDENTIALS_USR");
-	private String botPath = System.getenv("BOT_CREDENTIALS_PWD");
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		try {
 			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-			
-			telegramBotsApi.registerBot(new Team23BotController(botName,botPath),new SetWebhook("https://api.romongo.uk/" + botPath));
+			telegramBotsApi.registerBot(new Team23BotController(System.getenv("BOT_CREDENTIALS_PSW"), System.getenv("BOT_CREDENTIALS_USR")));
 		} catch (TelegramApiException e) {
 			e.printStackTrace();
 		}
 	}
-	*/
 
 }
--- src/main/java/com/javabot/controller/Team23BotController.java
+++ src/main/java/com/javabot/controller/Team23BotController.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -16,26 +16,51 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 @SuppressWarnings("deprecation")
 @Controller
 @RequestMapping(path="/bot")
-public class Team23BotController extends TelegramWebhookBot  {
+public class Team23BotController extends TelegramLongPollingBot  {
 
     private final static Logger logger = LoggerFactory.getLogger(Team23BotController.class);
-    private String botName = System.getenv("BOT_CREDENTIALS_USR");
+    private String botName;
     private String botPath = "https://api.romongo.uk/bot/extreme-bot-endpoint";    
-    private String botToken = System.getenv("BOT_CREDENTIALS_PSW");
+    private String botToken;
+
+    public Team23BotController(String botToken, String botName) {
+		super(botToken);
+		this.botName = botName;
+	}
+
     @Override
 	public String getBotUsername() {		
 		return botName;
 	}
-
+    /*
     @Override
     public String getBotPath() {
         return botPath;
     }
+    */
     @Override
     public String getBotToken(){
         return botToken;
     }
 
+    @Override
+    public void onUpdateReceived(Update update){
+        if (update.hasMessage() && update.getMessage().hasText()){
+            String messageTextFromTelegram = update.getMessage().getText();
+			long chatId = update.getMessage().getChatId();
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(String.valueOf(chatId));
+            sendMessage.setText(messageTextFromTelegram);
+
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                logger.error(e.getLocalizedMessage(), e);
+            }
+
+        }
+    }
+    /*
     @Override
     @PostMapping(path="/extreme-bot-endpoint")
     public BotApiMethod<?> onWebhookUpdateReceived(@RequestBody Update update) {
@@ -57,12 +82,13 @@ public class Team23BotController extends TelegramWebhookBot  {
         
         //String urlToSendMessage = "https://api.telegram.org/bot"+System.getenv("BOT_CREDENTIALS_PWD")+"/sendMessage?chat_id=" + chatId + "&text=" + messageTextFromTelegram;
         try {
-            execute(sendMessage);                       
+            execute(sendMessage);         
         } catch (TelegramApiException e) {
             logger.error(e.getLocalizedMessage(), e);     
         }
         return null;
     }
+    */
 
 
 }
