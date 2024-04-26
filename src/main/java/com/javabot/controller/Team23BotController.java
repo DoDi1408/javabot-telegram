9e4e8a501d2376766168c4676a28532bfe4c201e
DoDi1408 <ro.chavez.5454@gmail.com>
2024-04-26 15:01:55 -0600

    added bot token

--- src/main/java/com/javabot/controller/Team23BotController.java
+++ src/main/java/com/javabot/controller/Team23BotController.java
@@ -1,5 +1,7 @@
 package com.javabot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +10,7 @@ import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 @SuppressWarnings("deprecation")
@@ -15,8 +18,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @RequestMapping(path="/bot")
 public class Team23BotController extends TelegramWebhookBot  {
 
+    private final static Logger logger = LoggerFactory.getLogger(Team23BotController.class);
     private String botName = System.getenv("BOT_CREDENTIALS_USR");
-    private String botPath = "https://api.romongo.uk/bot/" + System.getenv("BOT_CREDENTIALS_PWD");    
+    private String botPath = "https://api.romongo.uk/bot/extreme-bot-endpoint";    
+    private String botToken = System.getenv("BOT_CREDENTIALS_PSW");
     @Override
 	public String getBotUsername() {		
 		return botName;
@@ -25,6 +30,10 @@ public class Team23BotController extends TelegramWebhookBot  {
     @Override
     public String getBotPath() {
         return botPath;
+    }
+    @Override
+    public String getBotToken(){
+        return botToken;
     }
 
     @Override
@@ -46,15 +55,12 @@ public class Team23BotController extends TelegramWebhookBot  {
         sendMessage.setText("Hello world");
         
         //String urlToSendMessage = "https://api.telegram.org/bot"+System.getenv("BOT_CREDENTIALS_PWD")+"/sendMessage?chat_id=" + chatId + "&text=" + messageTextFromTelegram;
-
-        return sendMessage;    
-        /* 
         try {
-            execute(sm);                        //Actually sending the message
+            execute(sendMessage);                       
         } catch (TelegramApiException e) {
-            logger.error(e.getLocalizedMessage(), e);      //Any error will be printed here
+            logger.error(e.getLocalizedMessage(), e);     
         }
-        */
+        return null;
     }
 
 
