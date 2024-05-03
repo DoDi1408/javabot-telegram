489ff44150e676b91110120dd2226510899c7d7e
DoDi1408 <ro.chavez.5454@gmail.com>
2024-05-03 15:13:21 -0600

    modified commands

--- src/main/java/com/javabot/util/BotCommands.java
+++ src/main/java/com/javabot/util/BotCommands.java
@@ -3,19 +3,19 @@ package com.javabot.util;
 public enum BotCommands {
 
 	START_COMMAND("/start"), 
-	REGISTER_EMP_COMMAND("/registerEmployee"), 
-	REGISTER_MAN_COMMAND("/registerManager"),
+	REGISTER_EMP_COMMAND("/registeremployee"), 
+	REGISTER_MAN_COMMAND("/registermanager"),
 	REGISTER_MANAGER("REGISTER_MANAGER"),
-	JOIN_TEAM("/joinTeam"),
+	JOIN_TEAM("/jointeam"),
 	JOIN_TEAM_IMP("JOIN_TEAM"),
-	TODO_LIST("/todoList"),
-	TEAM_LIST("/teamList"),
+	TODO_LIST("/todolist"),
+	TEAM_LIST("/teamlist"),
 	ADD_ITEM("/additem"),
 	ADD_TASK("ADD_TASK"),
-	UPDATE_TASK("/updateTask"),
+	UPDATE_TASK("/updatetask"),
 	ADVANCE_TASK("PROCEED_TASK"),
 	REVERT_TASK("REVERT_TASK"),
-	DELETE_COMMAND("/deleteTask"),
+	DELETE_COMMAND("/deletetask"),
 	DELETE_TASK("DELETE_TASK");
 
 	private String command;
