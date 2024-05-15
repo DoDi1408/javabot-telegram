209b5233db37d8de850ff02afadeb0305a7d5fc6
DoDi1408 <ro.chavez.5454@gmail.com>
2024-05-15 16:41:21 -0600

    deleted connection test as it was unnecessary

--- src/test/java/com/javabot/TelegramBotTeam23ApplicationTests.java
+++ src/test/java/com/javabot/TelegramBotTeam23ApplicationTests.java
@@ -1,24 +1,12 @@
 package com.javabot;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.javabot.serviceimp.EmployeeRepository;
 
 @SpringBootTest
 class TelegramBotTeam23ApplicationTests {
-	
-	@Autowired
-  	private EmployeeRepository employeeRepository;
 
     @Test
 	void contextLoads() {
-		try {
-			employeeRepository.testConnection();
-        } catch (Exception e) {
-            System.out.println("EXCEPTION WHILE TESTING DATABASE CONNECTION: " + e.getMessage());
-            throw new RuntimeException("I throw this error IF database connection fails so that the build fails, sorry, check database config", e);
-        }
 	}
 	
 }
