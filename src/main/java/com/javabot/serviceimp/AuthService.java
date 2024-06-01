22f206a56b9506de211a04813f9d28b3b5fa9055
DoDi1408 <ro.chavez.5454@gmail.com>
2024-05-31 18:33:40 -0600

    i am ded

--- src/main/java/com/javabot/serviceimp/AuthService.java
+++ src/main/java/com/javabot/serviceimp/AuthService.java
@@ -3,6 +3,7 @@ package com.javabot.serviceimp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -39,6 +40,9 @@ public class AuthService {
         catch (HttpServerErrorException seex){
             loggerAuthService.error("5xx Error Code",seex);
             return ResponseEntity.status(seex.getStatusCode()).body(seex.getResponseBodyAsString());
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
         }
     }
 }
